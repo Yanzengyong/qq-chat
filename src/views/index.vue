e2549481698768194776a1bde0532ba388a802f1
地瓜 <hellodigua@gmail.com>
2017-10-21 02:52:08 +0800

    创建聊天窗口头结构

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,15 +1,47 @@
 <template>
-  <div>
-    this is index page
+  <div id="chat-room">
+    <div class="head">
+      <div class="info">
+        <div class="avatar">
+          <img :src="you.imgUrl" draggable="false">
+          <p class="name">{{you.name}}</p>
+          <p class="state">{{you.state}}</p>
+        </div>
+      </div>
+      <div class="menu">
+        <div class="item close"></div>
+        <div class="item"></div>
+        <div class="item"></div>
+        <div class="item"></div>
+      </div>
+      <div class="tools">
+        <div class="item"></div>
+        <div class="item"></div>
+      </div>
+    </div>
+    <div class="content"></div>
+    <div class="message"></div>
   </div>
 </template>
+
 <script>
 export default {
   data () {
     return {
+      you: {
+        name: '对方',
+        state: '这里应该是状态',
+        imgUrl: '',
+        msg: '待发送的消息'
+      },
+      me: {
+        imgUrl: '',
+        msg: ''
+      }
     }
   }
 }
 </script>
 <style lang="scss" scoped>
+
 </style>
