4607d6fed03b96aae58138f65b7d6da369a72bcd
地瓜 <hellodigua@gmail.com>
2017-10-21 02:06:54 +0800

    init project with vue-cli

---
+++ src/views/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    this is index page
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+</style>
