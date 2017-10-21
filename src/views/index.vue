01482f0ff9a2b601cdaf2c7a732deebe65407a9f
地瓜 <hellodigua@gmail.com>
2017-10-22 02:34:14 +0800

    图片引入方式改为相对路径

--- src/views/index.vue
+++ src/views/index.vue
@@ -60,94 +60,94 @@ export default {
         tools: [
           {
             name: '音频',
-            src: '/static/images/tools/toobar_audio.png'
+            src: './static/images/tools/toobar_audio.png'
           },
           {
             name: '视频',
-            src: '/static/images/tools/toobar_video.png'
+            src: './static/images/tools/toobar_video.png'
           },
           {
             name: '远程演示',
-            src: '/static/images/tools/toobar_screen_share.png'
+            src: './static/images/tools/toobar_screen_share.png'
           },
           {
             name: '传送文件',
-            src: '/static/images/tools/toobar_send.png'
+            src: './static/images/tools/toobar_send.png'
           },
           {
             name: '远程控制',
-            src: '/static/images/tools/toobar_remote.png'
+            src: './static/images/tools/toobar_remote.png'
           },
           {
             name: '发起多人聊天',
-            src: '/static/images/tools/toobar_add.png'
+            src: './static/images/tools/toobar_add.png'
           },
           {
             name: '应用',
-            src: '/static/images/tools/toobar_app.png'
+            src: './static/images/tools/toobar_app.png'
           }
         ],
         menu: [
           {
             name: '设置',
-            src: '/static/images/menu/setting.png'
+            src: './static/images/menu/setting.png'
           },
           {
             name: '缩小',
-            src: '/static/images/menu/min.png'
+            src: './static/images/menu/min.png'
           },
           {
             name: '放大',
-            src: '/static/images/menu/max.png'
+            src: './static/images/menu/max.png'
           },
           {
             name: '关闭',
-            src: '/static/images/menu/close.png'
+            src: './static/images/menu/close.png'
           }
         ],
         msgTools: [
           {
             name: '字体',
-            src: '/static/images/msg-tools/font.png'
+            src: './static/images/msg-tools/font.png'
           },
           {
             name: '表情',
-            src: '/static/images/msg-tools/face.png'
+            src: './static/images/msg-tools/face.png'
           },
           {
             name: 'GIF',
-            src: '/static/images/msg-tools/gif.png'
+            src: './static/images/msg-tools/gif.png'
           },
           {
             name: '窗口抖动',
-            src: '/static/images/msg-tools/twitter.png'
+            src: './static/images/msg-tools/twitter.png'
           },
           {
             name: '语音消息',
-            src: '/static/images/msg-tools/record.png'
+            src: './static/images/msg-tools/record.png'
           },
           {
             name: '图片',
-            src: '/static/images/msg-tools/sendpic.png'
+            src: './static/images/msg-tools/sendpic.png'
           },
           {
             name: '音乐',
-            src: '/static/images/msg-tools/music.png'
+            src: './static/images/msg-tools/music.png'
           },
           {
             name: '截图',
-            src: '/static/images/msg-tools/cut.png'
+            src: './static/images/msg-tools/cut.png'
           },
           {
             name: '红包',
-            src: '/static/images/msg-tools/hb.png'
+            src: './static/images/msg-tools/hb.png'
           }
         ],
-        more: '/static/images/tools/more.png'
+        more: './static/images/tools/more.png'
       },
       you: {
         name: '夜喵。',
-        imgUrl: '/static/images/avatar_2.jpg',
+        imgUrl: './static/images/avatar_2.jpg',
         msg: '待发送的消息'
       },
       me: {
